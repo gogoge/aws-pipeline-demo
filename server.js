8efc55c1e18b2e6b45b99bd563ba626c79d11d51
Webb Lu <tsung.ying.lu@gmail.com>
2017-02-10 11:25:20 +0800

    update webpack-dev-middleware config

--- server.js
+++ server.js
@@ -44,7 +44,24 @@ function applyMiddleware(middleware, req, res) {
 }
 
 app.use(koaWebpackMiddleware(compiler, {
+  // display no info to console (only warnings and errors)
+  noInfo: false,
+  // display nothing to the console
+  quiet: false,
+  // switch into lazy mode
+  // that means no watching, but recompilation on every request
+  lazy: true,
+  // watch options (only lazy: false)
+  watchOptions: {
+      aggregateTimeout: 300,
+      poll: true
+  },
+  // custom headers
+  headers: { "X-Custom-Header": "yes" },
+  // public path to bind the middleware to
+  // use the same as in webpack
   publicPath: webpackConfig.output.publicPath,
+  // options for formating the statistics
   stats: { colors: true }
 }))
 // 1. static folder
